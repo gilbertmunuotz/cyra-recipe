bf65e8f2b90fd305cc23256ecf86091f71c4819f
gilbertmunuotz <officialgilbert24@gmail.com>
2024-03-05 20:13:18 -0800

    nothing..

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
     return (
         <div className='Footer'>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
 
-
-
+            </div>
         </div>
     )
 }
