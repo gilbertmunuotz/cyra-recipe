bfb421aa5e29e126c58eedb2e07e14b19f5bbc7b
gilbertmunuotz <128461895+gilbertmunuotz@users.noreply.github.com>
2024-05-31 08:46:30 -0700

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <div className='Navbar fixed top-0 w-full z-10'>
             <div className="flex justify-between items-center h-16 px-4 backdrop-blur-md">
                 <Link to={'/'}>
-                    <div className="text-3xl text-orange-500">Welcome</div>
+                    <div className="text-3xl text-orange-500">Cyra Recipe</div>
                 </Link>
             </div>
         </div>
